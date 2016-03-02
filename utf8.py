0aebf1cbd673cba9e4debf3618989c022c83ed63
unknown <lius@CPEKW-Q1100534.is.ad.igt.com>
2016-03-02 16:01:53 +0800

    s2= s.encode("gbk",errors="ignore").decode('gbk') chcp 65001

--- utf8.py
+++ utf8.py
@@ -1,5 +1,7 @@
 from urllib.request import urlopen
 textPage = urlopen(
 "http://www.pythonscraping.com/pages/warandpeace/chapter1-ru.txt")
-a=(str(textPage.read(), 'utf-8'))#如果print会失败，但是交互式显式a是成功的
-print(a)#只可以print a，如果textPage.read是不行的，它已经被读过了，空了
+s=(str(textPage.read(), 'utf-8'))#如果print会失败，但是交互式显式a是成功的
+#print(s)#只可以print a，如果textPage.read是不行的，它已经被读过了，空了
+s2= s.encode("gbk",errors="ignore").decode('gbk')
+print('now is chinese: \n',s2)
